670c21d717bd7e43bf6ad0a9dbf0c4c73044a67f
MickMAGI <greenday.chon@gmail.com>
2019-01-23 16:25:39 +0900

    Initial Commit

---
+++ MyTime スマホ依存解消サポートアプリ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTime スマホ依存解消サポートアプリ
+//
+//  Created by Mikihito on 2019/01/23.
+//  Copyright © 2019 Mikihito Tamaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
